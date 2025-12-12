0566f02d2814975f344ae8aa744813bc5d7de37c
Miriam-Nav <marnavvid2@alu.edu.gva.es>
2025-12-12 12:29:30 +0100

    LoginScreen

---
+++ Moveo/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { PaperProvider } from 'react-native-paper';
+import LoginScreen from './src/app/LoginScreen';
+import theme from './src/theme';
+
+export default function App() {
+  return (
+    <PaperProvider theme={theme}>
+      <LoginScreen />
+    </PaperProvider>
+  );
+}
+
---
+++ Moveo/src/theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    primary: '#100f13ff', 
+    onPrimary: '#FFFFFF',      
+    secondary: '#eac50dff',
+    onSecondary: '#100f13ff',
+    error: '#B00020',     
+    background: '#333333',
+    text: '#333333',
+    inputText: '#666666',
+    placeholder: '#999999',
+    surface: '#FFFFFF',
+    backgroundCard: '#FFFFFF',
+    outline: '#D1D5DB',
+  },
+};
+
+export default theme;
